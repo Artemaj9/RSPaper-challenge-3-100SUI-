ea1b0b9e7fddf65a0b1df07f230985498a2de57b
Artem <moiseev@physics.msu.ru>
2023-07-19 17:53:51 +0300

    Initial Commit

---
+++ RSPaper(challenge 3 100SUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RSPaper(challenge 3 100SUI)
+//
+//  Created by Artem on 19.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RSPaper(challenge 3 100SUI)/RSPaper_challenge_3_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  RSPaper_challenge_3_100SUI_App.swift
+//  RSPaper(challenge 3 100SUI)
+//
+//  Created by Artem on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RSPaper_challenge_3_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
